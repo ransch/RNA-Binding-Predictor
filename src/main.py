573084926c9a204ef173e7191a4b0a45b8e1dac4
Ran Schreiber <ransch7@gmail.com>
2024-08-25 23:19:39 +0300

    a few changes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
+import os
 import pathlib
 import sys
-import os
 
 import keras
 import numpy as np
@@ -9,13 +9,14 @@ from keras import layers
 from keras import optimizers
 from keras import regularizers
 
-from average_last_cycles import AverageLast
-from dna_dataset_utils import augment_dataset
+# from average_last_cycles import AverageLast
+# from dna_dataset_utils import augment_dataset
 from rna_compete_dataset import rna_compete_dataset
 from selex_dataset import combined_selex_dataset
 from time_limit import TimeLimitCallback
 
-SAVED_MODEL_PATH = './RNA-Binding-Predictor/saved_model.keras'
+_SHOULD_SAVE_MODEL = False
+_SAVED_MODEL_PATH = './saved_model.keras'
 
 _MAX_EPOCHS_NUM = 1000
 _STEPS_PER_EPOCH = 2048
@@ -23,24 +24,13 @@ _BATCH_SIZE = 64
 _PREDICTION_BATCH_SIZE = 512
 _VALIDATION_DATA_SIZE = 32768
 _MIN_ACCURACY_IMPROVEMENT_DELTA = .0001
-_ACCURACY_IMPROVEMENT_PATIENCE = 5
 _L2_REGULARIZATION_FACTOR = .01
-_LEAKY_RELU_SLOPE = .1
+_LEAKY_RELU_SLOPE = .01
 _MAX_MINUTES_TIME_LIMIT = 55
 _ADAM_LEARNING_RATE = 0.002
 
-MAX_SEQUENCE_LENGTH = 40
-NUM_CLASSES = 4
-FILTERS = 64
-KERNEL_SIZE = 5
-POOL_SIZE = 2
-DENSE_UNITS_1 = 128
-DENSE_UNITS_2 = 64
-NUM_HEADS = 4
-KEY_DIM = 64
 
-
-def _get_model():
+def _get_model(max_given_cycle):
     """
     Get the binding prediction model.
 
@@ -51,24 +41,27 @@ def _get_model():
          The prediction model.
     """
     model = keras.Sequential()
+    model.add(layers.Input(shape=(None, 4)))
 
     # LSTM layers with dropout and regularization
-    model.add(layers.LSTM(256, return_sequences=True, input_shape=(40, 4),
-                          kernel_regularizer=regularizers.l2(0.01)))
-    model.add(layers.LSTM(128, kernel_regularizer=regularizers.l2(0.01)))
+    model.add(layers.LSTM(256, return_sequences=True,
+                          kernel_regularizer=regularizers.l2(_L2_REGULARIZATION_FACTOR)))
+    model.add(layers.LSTM(128, kernel_regularizer=regularizers.l2(_L2_REGULARIZATION_FACTOR)))
     model.add(layers.Dropout(0.5))
 
     # Dense layers with Batch Normalization and LeakyReLU activation
-    model.add(layers.Dense(128, kernel_regularizer=regularizers.l2(0.01)))
+    model.add(layers.Dense(128, kernel_regularizer=regularizers.l2(_L2_REGULARIZATION_FACTOR)))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(negative_slope=_LEAKY_RELU_SLOPE))
 
-    model.add(layers.Dense(64, kernel_regularizer=regularizers.l2(0.01)))
+    model.add(layers.Dense(64, kernel_regularizer=regularizers.l2(_L2_REGULARIZATION_FACTOR)))
     model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU(negative_slope=0.01))
+    model.add(layers.LeakyReLU(negative_slope=_LEAKY_RELU_SLOPE))
 
     # Output layer for classification
     model.add(layers.Dense(5, activation='softmax'))
+
+    # model.add(AverageLast(5 - max_given_cycle))
 
     # Compile the model
     model.compile(optimizer=optimizers.Adam(learning_rate=_ADAM_LEARNING_RATE),
@@ -78,11 +71,27 @@ def _get_model():
     return model
 
 
-callbacks_list = [
-    keras.callbacks.EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True),
-    keras.callbacks.ModelCheckpoint(filepath=SAVED_MODEL_PATH, monitor='val_loss', save_best_only=True),
-    keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, min_lr=1e-6)
+_TRAINING_CALLBACKS_LIST = [
+    keras.callbacks.ReduceLROnPlateau(
+        monitor='val_accuracy',
+        mode='max', factor=0.5,
+        patience=5,
+        min_lr=1e-6),
+    TimeLimitCallback(max_minutes=_MAX_MINUTES_TIME_LIMIT),
+    keras.callbacks.EarlyStopping(
+        monitor='val_accuracy',
+        mode='max',
+        min_delta=_MIN_ACCURACY_IMPROVEMENT_DELTA,
+        patience=10,
+        restore_best_weights=True)
 ]
+
+if _SHOULD_SAVE_MODEL:
+    _TRAINING_CALLBACKS_LIST.append(
+        keras.callbacks.ModelCheckpoint(
+            filepath=_SAVED_MODEL_PATH,
+            monitor='val_loss',
+            save_best_only=True))
 
 
 def _results_to_binding_predictions(model_results):
@@ -132,7 +141,8 @@ def _get_selex_dataset(selex_file_paths, validation_data_size):
          A pair of the training and validation sets.
     """
     train_ds, val_ds = combined_selex_dataset(selex_file_paths, validation_data_size)
-    train_ds = train_ds.padded_batch(  # augment_dataset(train_ds).padded_batch(
+    # train_ds = augment_dataset(train_ds)
+    train_ds = train_ds.padded_batch(
         batch_size=_BATCH_SIZE,
         padded_shapes=([None, 4], []),
         padding_values=0)
@@ -169,57 +179,31 @@ def _get_tensors(dataset):
     return features, labels
 
 
-def _use_saved_model_for_rna_compete_ds(output_file_path, rna_compete_file_path):
-    model = keras.models.load_model(SAVED_MODEL_PATH)
-    # Evaluate the model on the RNAcompete dataset, and translate the model's outputs into binding
-    # predictions.
-    rna_compete_ds = rna_compete_dataset(rna_compete_file_path).padded_batch(
-        batch_size=_PREDICTION_BATCH_SIZE, padded_shapes=[None, 4], padding_values=0)
-    model_results = model.predict(rna_compete_ds)
-    predictions = _results_to_binding_predictions(model_results)
-
-    # Write the predictions to a file.
-    _write_predictions(output_file_path, predictions)
-
-
 def main():
     output_file_path = sys.argv[1]
     rna_compete_file_path = sys.argv[2]
 
-    # use saved model to adjust _results_to_binding_predictions:
-    if os.path.exists(SAVED_MODEL_PATH):
-        print("Loading saved model...")
-        _use_saved_model_for_rna_compete_ds(output_file_path, rna_compete_file_path)
-        return
-
-    # if no saved model, follow original workflow:
-    selex_file_paths = _parse_selex_file_paths(sys.argv[3:])
-    max_given_cycle = max(selex_file_paths.keys())
-
-    train_ds, val_ds = _get_selex_dataset(selex_file_paths, _VALIDATION_DATA_SIZE)
     rna_compete_ds = rna_compete_dataset(rna_compete_file_path).padded_batch(
         batch_size=_PREDICTION_BATCH_SIZE, padded_shapes=[None, 4], padding_values=0)
-    model = _get_model()
 
-    time_limit_callback = TimeLimitCallback(max_minutes=_MAX_MINUTES_TIME_LIMIT)
-    early_stopping_callback = keras.callbacks.EarlyStopping(
-        monitor='val_accuracy',
-        mode='max',
-        min_delta=_MIN_ACCURACY_IMPROVEMENT_DELTA,
-        patience=_ACCURACY_IMPROVEMENT_PATIENCE,
-        restore_best_weights=True)
+    if _SHOULD_SAVE_MODEL and os.path.exists(_SAVED_MODEL_PATH):
+        print("Loading saved model...")
+        model = keras.models.load_model(_SAVED_MODEL_PATH)
+    else:
+        selex_file_paths = _parse_selex_file_paths(sys.argv[3:])
+        max_given_cycle = max(selex_file_paths.keys())
 
-    # Fit the model using the SELEX dataset, limiting the training time and stopping if the
-    # validation accuracy stops improving.
-    model.fit(train_ds,
-              epochs=_MAX_EPOCHS_NUM,
-              steps_per_epoch=_STEPS_PER_EPOCH,
-              callbacks=[callbacks_list],
-              validation_data=_get_tensors(val_ds),
-              validation_batch_size=_PREDICTION_BATCH_SIZE)
+        train_ds, val_ds = _get_selex_dataset(selex_file_paths, _VALIDATION_DATA_SIZE)
+        model = _get_model(max_given_cycle)
 
-    #  by default, save a model after it was trained
-    model.save(SAVED_MODEL_PATH)
+        # Fit the model using the SELEX dataset, limiting the training time and stopping if the
+        # validation accuracy stops improving.
+        model.fit(train_ds,
+                  epochs=_MAX_EPOCHS_NUM,
+                  steps_per_epoch=_STEPS_PER_EPOCH,
+                  callbacks=[_TRAINING_CALLBACKS_LIST],
+                  validation_data=_get_tensors(val_ds),
+                  validation_batch_size=_PREDICTION_BATCH_SIZE)
 
     # Evaluate the model on the RNAcompete dataset, and translate the model's outputs into binding
     # predictions.
